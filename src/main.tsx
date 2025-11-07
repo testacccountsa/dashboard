1bb6123b5b9f36bb346eaed28825d5ac68ae6737
saba <sabapathi.t-external@alfuttaim.com>
2025-11-07 15:37:29 +0400

    cs  import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import { createRoot } from "react-dom/client";
 // import App from "./App.tsx";
-// import "./index.css";
+import "./index.css";
 
 // // Detect if running in iframe and add class to body
 // if (window.self !== window.top) {
